455f5667760f0112964989a16fdd9ad0db3398af
okanogutlu <okanogutlu@users.noreply.github.com>
2020-06-29 15:24:01 +0300

    GPA calculator app
    made with Android Studio

---
+++ smstr/app/src/main/java/com/example/smstr/hesapla.java
@@ -0,0 +1,53 @@
+package com.example.smstr;
+
+import android.widget.EditText;
+
+public class hesapla extends MainActivity{
+/*
+    static double cevir(String harf){
+        if(harf.equals("AA")||harf.equals("Aa")||harf.equals("aA")||harf.equals("aa")){
+            return 4.0;
+        }
+        if(harf.equals("BB")||harf.equals("Bb")||harf.equals("bB")||harf.equals("bb")){
+            return 3.0;
+        }
+        if(harf.equals("Ba")||harf.equals("BA")||harf.equals("ba")||harf.equals("bA")||harf.equals("AB")||harf.equals("ab")||harf.equals("aB")||harf.equals("Ab")){
+            return 3.5;
+        }
+        if(harf.equals("BC")||harf.equals("Bc")||harf.equals("bC")||harf.equals("bc")||harf.equals("CB")||harf.equals("Cb")||harf.equals("cB")||harf.equals("cb")){
+            return 2.5;
+        }
+        if(harf.equals("CC")||harf.equals("Cc")||harf.equals("cC")||harf.equals("cc")){
+            return 2.0;
+        }
+        if(harf.equals("CD")||harf.equals("Cd")||harf.equals("cD")||harf.equals("cd")||harf.equals("DC")||harf.equals("Dc")||harf.equals("dC")||harf.equals("dc")){
+            return 1.5;
+        }
+
+        if(harf.equals("DD")||harf.equals("Dd")||harf.equals("dD")||harf.equals("dd")){
+            return 1.0;
+        }
+        if(harf.equals("FF")||harf.equals("Ff")||harf.equals("fF")||harf.equals("ff")){
+            return 0.0;
+        }
+        return 0.0;
+    }
+    double sonuc(){
+
+
+         double toplam=10;
+         String s;
+         int i=0;
+         for(i=0;i<4;i++){
+
+              s= String.valueOf(K[i].getText());
+             toplam+=(Double.parseDouble(s)*tr[i]);
+                System.out.println(toplam);
+         }
+        toplam=toplam/i;
+         return toplam;
+
+    }
+    */
+
+}
